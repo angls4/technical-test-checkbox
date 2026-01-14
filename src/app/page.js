e418c6308956b5dc2da877a6c46f01c5a314bfef
adnansqiva <adnan.majid@sqiva.com>
2026-01-14 14:16:19 +0700

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,183 @@
-import Image from "next/image";
+"use client";
+
+// imports
+import { forwardRef, useState, createContext, useEffect, useContext } from "react";
+
+const PressedClassWrapper = ({ children, className, ...props }) => {
+  const [isPressed, setIsPressed] = useState(false);
+
+  function onMouseDown(e) {
+    e.stopPropagation();
+    setIsPressed(true);
+  }
+
+  function onMouseUp(e) {
+    e.stopPropagation();
+    setIsPressed(false);
+  }
+
+  function onMouseLeave(e) {
+    e.stopPropagation();
+    setIsPressed(false);
+  }
+
+  return (
+    <div onMouseDown={onMouseDown} onMouseUp={onMouseUp} onMouseLeave={onMouseLeave} className={(isPressed ? "pressed " : "") + (className ? className : "")} {...props}>
+      {children}
+    </div>
+  );
+}
+
+const CheckMarkIcon = ({className}) => (
+  <svg width="17" height="13" viewBox="0 0 17 13" className={className} fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M0.5 6.572L6.04879 11.5072C6.06925 11.5254 6.10055 11.5237 6.11899 11.5035L16.14 0.5" stroke="currentColor" strokeLinecap="round"/>
+  </svg>
+);
+
+const Checkbox = forwardRef(function Checkbox(
+  { className, ...props },
+  ref
+) {
+  return (
+    <label>
+      <PressedClassWrapper className={"checkbox-wrapper inline-flex items-center cursor-pointer " + (className ? className : "")}>
+          <input
+            ref={ref}
+            type="checkbox"
+            {...props}
+          />
+          <div className="checkbox-box">
+            <CheckMarkIcon className="checkbox-checkmark" />
+          </div>
+      </PressedClassWrapper>
+    </label>
+  );
+});
+
+const Separator = ({className}) => (
+  <div className={"separator px-3.75 py-[9.5px] " + (className ? className : "")}>
+    <svg width="340" height="1" viewBox="0 0 340 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <line x1="0.35" y1="0.349997" x2="339.65" y2="0.349997" stroke="currentColor" strokeWidth="0.7" strokeLinecap="round"/>
+    </svg>
+  </div>
+  // <div className={"px-3.75 my-2.5 " + (className ? className : "")}>
+  //   <div className="separator"></div>
+  // </div>
+);
+
+const Button = ({className, children, ...props}) => {
+  return (
+    <PressedClassWrapper className={"btn flex justify-center items-center " + (className ? className : "")} {...props}>
+      {children}
+    </PressedClassWrapper>
+  );
+}
+
+const ChecklistItemAll = ({label,name}) => {
+  const context = useContext(ChecklistContext);
+  const [checkAll, setCheckAll] = [context.checkAll, context.setCheckAll];
+
+  function handleChange(e) {
+    setCheckAll(e.target.checked);
+
+    if (e.target.checked) {
+      context.setCheckCount(0);
+      setCheckAll(true);
+    }
+    else {
+      context.setCheckCount(context.itemCount);
+      setCheckAll(false);
+    }
+  }
+
+  return (
+    <li>
+      <label>
+        <PressedClassWrapper className="checkbox-wrapper h-full flex justify-between items-center gap-0 cursor-pointer">
+          <span>{label}</span>
+          <Checkbox name={name} className="mr-5.25" onChange={handleChange} checked={checkAll} />
+        </PressedClassWrapper>
+      </label>
+    </li>
+  )
+}
+
+const ChecklistItem = ({label,name}) => {
+  const [checked, setChecked] = useState(false);
+  const context = useContext(ChecklistContext);
+
+  useEffect(() => {
+    if (context.checkAll) {
+      setChecked(true);
+    }
+    else if (context.checkCount === context.itemCount) {
+      setChecked(false);
+    }
+  }, [context.checkAll]);
+
+  function handleChange(e) {
+    setChecked(e.target.checked);
+
+    if (e.target.checked) {
+      context.setCheckCount(context.checkCount - 1);
+      if (context.checkCount === 1) {
+        context.setCheckAll(true);
+      }
+    }
+    else {
+      context.setCheckCount(context.checkCount + 1);
+      if (context.checkAll)
+        context.setCheckAll(false);
+    }
+  }
+
+  return (
+    <li>
+      <label>
+        <PressedClassWrapper className="checkbox-wrapper h-full flex justify-between items-center gap-0 cursor-pointer">
+          <span>{label}</span>
+          <Checkbox name={name} className="mr-5.25" onChange={handleChange} checked={checked} />
+        </PressedClassWrapper>
+      </label>
+    </li>
+  )
+}
+
+const ChecklistContext = createContext();
+
+const ChecklistContainer = ({ data }) => {
+  const [checkAll, setCheckAll] = useState(false);
+  const [checkCount, setCheckCount] = useState(data.length);
+
+  return (
+    <ChecklistContext.Provider value={{checkAll, setCheckAll, checkCount, setCheckCount, itemCount: data.length}}>
+      <ul className="checklist-container flex flex-col items-center justify-start">
+        <ChecklistItemAll label="All pages" name="all-pages" />
+        <Separator className="w-full" />
+        <div className="h-41 overflow-y-scroll no-scrollbar">
+          {
+            data.map((item, index) => {
+              return (
+                <ChecklistItem key={index} label={item} name={"page-" + (index+1)} />
+              );
+            })
+          }
+        </div>
+        <Separator className="w-full" />
+        <Button className="my-2.5">
+          <span>Done</span>
+        </Button>
+      </ul>
+    </ChecklistContext.Provider>
+  );
+}
 
 export default function Home() {
+  const data = ["Page 1", "Page 2", "Page 3", "Page 4", "Page 5", "Page 6"];
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="h-dvh flex items-center justify-center bg-white">
+      <ChecklistContainer data={data} />
     </div>
   );
 }
